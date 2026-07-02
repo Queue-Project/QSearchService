8e9f11dc7793c57e503c5f56caa204b465ea374b
BakhtiyorSharipov <bsharipov731@gmail.com>
2026-07-02 12:53:23 +0500

    implemented-entity-configurations-and-migration

---
+++ QSearchService.Infrastructure/Persistence/Database/SearchServiceDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using QSearchService.Domain.Models;
+using QSearchService.Infrastructure.Persistence.TableConfiguration;
+
+namespace QSearchService.Infrastructure.Persistence.Database;
+
+public class SearchServiceDbContext: DbContext
+{
+
+    public DbSet<SearchVectorDocument> SearchVectorDocuments { get; set; }
+
+    public SearchServiceDbContext(DbContextOptions<SearchServiceDbContext> options): base(options)
+    {
+        
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SearchVectorDocumentTableConfiguration).Assembly);
+    }
+}
